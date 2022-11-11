5c40289c0664deeef414e8f90f3419980b569827
Jaron <jaronchretien@yahoo.com>
2022-11-10 22:59:33 -0600

    Create Post

--- db/index.js
+++ db/index.js
@@ -1,38 +1,71 @@
 //Imports the pg module
-const { Client } = require('pg'); //imports the pg module
+const { Client } = require("pg"); //imports the pg module
 
 //This gives the DB ame and location of the database
-const client = new Client('postgres://localhost:5432/juicebox-dev');
-
-
+const client = new Client("postgres://localhost:5432/juicebox-dev");
 
 const getAllUsers = async () => {
-    const {rows} = await client.query(
-        `SELECT id, username
+  const { rows } = await client.query(
+    `SELECT id, username, name, location, active           x 
          FROM users;
-         `)
-         return  rows
-}
+         `
+  );
+  console.log("Rows from Users", rows)
+  return rows;
+}; 
 
-const createUser = async({username, password}) => {
-    try {
-        const result = await client.query(`
-        INSERT INTO users(username, password) 
-        VALUES ($1, $2)
+const createUser = async ({ username, password, name, location }) => {
+  try {
+    const {rows: [user]} = await client.query(
+      `
+        INSERT INTO users(username, password, name, location) 
+        VALUES ($1, $2, $3, $4)
         ON CONFLICT (username) DO NOTHING
         RETURNING *;
-        `, [username, password]);
-      
-        
-    return rows
-    }catch(error) {
-        throw error;
-    }
-    }
+        `,
+      [username, password, name, location]
+    );
+   return user
+  } catch (error) {
+    throw error;
+  }
+};
+
+
+const updateUser = async (id, fields = {}) => {
+  //Build the set string
+  const setString = Object.keys(fields)
+    .map((key, index) => `"${key}"=$${index + 1} `)
+    .join(", ");
+
+  if (setString.length === 0) {
+    return;
+  }
+  console.log("Updating Users!")
+  try {
+    const {rows: [user]} = await client.query(
+      `
+            UPDATE users
+            SET ${setString}
+            WHERE id =  ${id}
+            RETURNING *
+            `,
+      Object.values(fields)
+    );
+    return user
+    
+  } catch (error) {
+    console.log("There was an error updating users!")
+    throw error;
+  }
+};
+
+
+
 
 module.exports = {
     client,
     getAllUsers,
-    createUser
-}
-
+    createUser,
+    updateUser,
+  };
--- db/seed.js
+++ db/seed.js
@@ -1,7 +1,4 @@
-const { client, 
-        getAllUsers,
-        createUser 
-} = require("./index");
+const { client, getAllUsers, createUser, updateUser,  } = require("./index");
 //Grabs client from index.js module exports
 
 //Calls a query which drops(deletes) all tables from out database
@@ -9,6 +6,7 @@ const dropTables = async () => {
   try {
     console.log("Starting to drop tables....");
     await client.query(`
+     DROP TABLE IF EXISTS posts;
       DROP TABLE IF EXISTS users;
     `);
     console.log("Finished dropping tables!");
@@ -24,7 +22,10 @@ const createTables = async () => {
         CREATE TABLE users (
             id SERIAL PRIMARY KEY,
             username varchar(255) UNIQUE NOT NULL,
-            password varchar(255) NOT NULL
+            password varchar(255) NOT NULL,
+            name VARCHAR(255) NOT NULL,
+            location VARCHAR(255) NOT NULL,
+            active BOOLEAN DEFAULT true
         );
       `);
     console.log("Finished creating tables!");
@@ -33,26 +34,59 @@ const createTables = async () => {
   }
 };
 
+const createPost = async({
+  authordId,
+  title,
+  content
+  }) => {
+  try {
+  const result = await client.query(`
+  CREATE TABLE posts (
+    id SERIAL PRIMARY KEY,
+    "authorId" INTEGER REFERENCES users (id) NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    active BOOLEAN DEFAULT true
+  )
+  `)
+  console.log("Done creating Posts!")
+  console.log("Results from creating posts", result)
+  }catch(error) {
+    console.log("There was an error creating posts!")
+    throw error;
+  }
+  }
+
 const rebuildDB = async () => {
   try {
     //Connecst the client to the database
     client.connect();
+
     await dropTables();
     await createTables();
     await createInitialUsers();
-   
-    
+    await getAllUsers();
   } catch (error) {
     console.error(error);
-  } finally {
-    console.log(client.end());
+    // } finally {
+    //   console.log(client.end());
+    // }
   }
 };
 
 async function testDB() {
   try {
     console.log("Starting to test database...");
-
+    const users = await getAllUsers();
+    console.log("Starting to Update Users!");
+    const updatedUserResult = await updateUser(users[0].id, {
+      name: "Newname Sogood",
+      location: "Lestervilla, KY",
+    });
+    console.log("Creating Post...")
+    const createdPost =  await createPost(2, "Hello", "Hi There")
+    console.log("Finished updating Database");
+    console.log("Finished creating Post!")
     console.log("Finished database tests!");
   } catch (error) {
     console.error("Error testing database!");
@@ -60,25 +94,30 @@ async function testDB() {
   }
 }
 
-const createInitialUsers = async() => {
-try {
-    console.log("Starting to create users...")
-    await createUser({username:'albert', password: 'bertie99'})
-    await createUser({username: 'sandra', password: 'glamgal'})
-    console.log(albert)
-    console.log(albertTwo)
-    console.log('Finished creating users!')
-}catch(error) {
-    console.error('Error creating users!')
+const createInitialUsers = async () => {
+  try {
+    console.log("Starting to create users...");
+    await createUser({
+      username: "albert",
+      password: "bertie99",
+      name: "Albert",
+      location: "Brasil",
+    });
+    await createUser({
+      username: "sandra",
+      password: "glamgal",
+      name: "Sanda",
+      location: "Brasil",
+    });
+
+    console.log("Finished creating users!");
+  } catch (error) {
+    console.error("Error creating users!");
     throw error;
-}
-
-}
-
+  }
+};
 
 
-rebuildDB()
-.then(testDB)
-.catch(console.error)
-.finally(() => client.end());
 
+rebuildDB().then(testDB).catch(console.error);
+// .finally(() => client.end());
